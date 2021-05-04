ca08d407d8d803f50592f15abb79485dea1138ce
CokePokes <cokepokes@gmail.com>
2021-05-04 02:46:25 -0500

    removed xpc code and use CF

--- libtweakemail/libtweakemail.mm
+++ libtweakemail/libtweakemail.mm
@@ -45,7 +45,7 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter();
         [constructedDic setObject:subject forKey:@"subject"];
     if (body)
         [constructedDic setObject:body forKey:@"body"];
- 
+         
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:constructedDic options:NSJSONWritingPrettyPrinted error:nil];
     NSString *json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     
@@ -53,49 +53,6 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter();
 }
 
 @end
-
-
-static void sendEmail(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
-    NSData *data = [(__bridge NSString *)object dataUsingEncoding:NSUTF8StringEncoding];
-    NSMutableDictionary *constructedDic = [[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil] mutableCopy];
-    
-    xpc_connection_t client = xpc_connection_create_mach_service("com.cokepokes.libtweakemaild",
-                                                                 NULL,
-                                                                 0);
-    xpc_connection_set_event_handler(client, ^(xpc_object_t event) {
-        CPLog("ERROR: xpc_connection_set_event_handler");
-    });
-    xpc_connection_resume(client);
-    xpc_connection_send_message(client, [(NSDictionary*)constructedDic.copy XPCObject]);
-}
-
-
-CHConstructor // code block that runs immediately upon load
-{
-    @autoreleasepool {
-        if ([NSProcessInfo.processInfo.processName isEqualToString:@"SpringBoard"]){ //only want springboard to run server
-            static dispatch_once_t onceToken;
-            dispatch_once(&onceToken, ^{
-                void *handle = dlopen("/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation", RTLD_LAZY);
-                void *impl = NULL;
-                if (handle) {
-                    impl = dlsym(handle, "CFNotificationCenterGetDistributedCenter");
-                }
-                if (impl) { // Available.
-                    // Registration in receiver process.
-                    CFNotificationCenterAddObserver(CFNotificationCenterGetDistributedCenter(),
-                                                    NULL,
-                                                    sendEmail,
-                                                    CFSTR("com.cokepokes.libtweakemail"),
-                                                    NULL,
-                                                    CFNotificationSuspensionBehaviorDeliverImmediately);
-                    
-                }
-            });
-        }
-    }
-}
-
 
 
 
